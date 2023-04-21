a9fca0d8871579aedb8fe45637d188e9b30fe63d
nnnnQT <cortana@s007xcpu2552.lan>
2023-04-20 21:39:23 -0500

    updated work section with technologies

--- src/components/WorkSection.tsx
+++ src/components/WorkSection.tsx
@@ -5,7 +5,8 @@ const WorkSection = () => {
             <small className="text-lg">Some of the main highlights I'd like to share.</small>
             <ul className="p-10 m-0 mt-10 text-left list-none bg-white rounded-lg shadow-xl bg-opacity-70">
                 <li className="m-0 min-w-[250px] max-w-[600px]">
-                    <h4 className="mt-0 text-2xl">📈 Growth SEO</h4>
+                    <h4 className="mt-0 mb-0 mb-2 text-2xl">📈 Growth SEO</h4>
+                    <small className="block mb-6 font-bold">Typescript, NextJS, Firebase, NX, React, Redux, Material UI, Tailwind, Styled Components, Ruby On Rails, Webpack, Netlify, and Javascript</small>
                     <ul className="p-0 m-0 text-lg list-none sm:list-disc sm:pl-8">
                         <li className="mb-4">
                             Fully customizable rehydrated deployed bundles of minified and compiled Typescript to Javascript web components. Think of a search input to direct a user to another application or phone input to collect user's mobile numbers for starting account creation. To see the sample set up, checkout the <a href="https://github.com/y04nqt/deployable-web-components" target="_blank" rel="noreferrer">repository on github</a>.
@@ -20,7 +21,8 @@ const WorkSection = () => {
                     <p className="italic">Examples: <a href="https://www.wonolo.com" target="_blank" rel="noreferrer">www.wonolo.com</a> & <a href="https://jobs.wonolo.com" target="_blank" rel="noreferrer">jobs.wonolo.com</a></p> 
                 </li>
                 <li className="m-0 mt-8 ml-auto min-w-[250px] max-w-[600px]">
-                    <h4 className="mt-0 text-2xl">👩‍🏭 Gig Economy Job Requesting and Worker Tracking</h4>
+                    <h4 className="mt-0 mb-0 mb-2 text-2xl">👩‍🏭 Gig Economy Job Requesting and Worker Tracking</h4>
+                    <small className="block mb-6 font-bold">PHP, AWS, PSQL, Lambda, SNS & SQS, EC2, S3, React, React Native, Vue, Typescript, Javascript, CSS/SCSS, Tailwind, Material UI, Python, and JQuery</small>
                     <ul className="p-0 m-0 text-lg list-none sm:list-disc sm:pl-8">
                         <li className="mb-4">
                             Created many features including systems for requesting multi-day shifts with variable settings to determine categorization, micro-service operations + architecture, and UI/UX enhancement for employers and workers.
@@ -35,7 +37,8 @@ const WorkSection = () => {
                     <p className="italic">You can find what remains of the company at <a href="https://moonrise.works" target="_blank" rel="noreferrer">moonrise.works</a>.</p>
                 </li>
                 <li className="m-0 mt-8 min-w-[250px] max-w-[600px]">
-                    <h4 className="mt-0 text-2xl">☎️ Online Call Center Application</h4>
+                    <h4 className="mt-0 mb-0 mb-2 text-2xl">☎️ Online Call Center Application</h4>
+                    <small className="block mb-6 font-bold">React, PHP, PSQL, Twilio, Tailwind, Styled Components, and AWS</small>
                     <ul className="p-0 m-0 text-lg list-none sm:list-disc sm:pl-8">
                         <li className="mb-4">
                             I helped contribute new features and functionality to an online call center application for workers. This enabled workers to call a list of numbers from their computer using any web browser, read the script, and eventually complete their shift.
@@ -50,7 +53,8 @@ const WorkSection = () => {
                     <p className="italic">This technology was built for <a href="https://moonrise.works" target="_blank" rel="noreferrer">moonrise.works</a> as another business vertical to create jobs for people who needed flexible work.</p>
                 </li>
                 <li className="m-0 ml-auto mt-8 min-w-[250px] max-w-[600px]">
-                    <h4 className="mt-0 text-2xl">📑 Remote I-9 Verification for W2 Work Application</h4>
+                    <h4 className="mt-0 mb-0 mb-2 text-2xl">📑 Remote I-9 Verification for W2 Work Application</h4>
+                    <small className="block mb-6 font-bold">Python, Flask, PSQL, React, Material UI, Javascript, and WebRTC</small>
                     <ul className="p-0 m-0 text-lg list-none sm:list-disc sm:pl-8">
                         <li className="mb-4">
                             Created and contributed to a remote I-9 verification React & Python application for workers and employers at the beginning of the pandemic. It enabled a user to fill out the I-9 form without any help similar to a typeform setup, except intelligent pathing based on selection.
@@ -65,7 +69,8 @@ const WorkSection = () => {
                     <p className="italic">This technology was built for <a href="https://moonrise.works" target="_blank" rel="noreferrer">moonrise.works</a> as another business vertical to create more tools within the business's ecosystem.</p>
                 </li>
                 <li className="m-0 mt-8 min-w-[250px] max-w-[600px]">
-                    <h4 className="mt-0 text-2xl">🌐 Qlik Sense Web 'Mashups'</h4>
+                    <h4 className="mt-0 mb-0 mb-2 text-2xl">🌐 Qlik Sense Web 'Mashups'</h4>
+                    <small className="block mb-6 font-bold">React, Javascript, JQuery, Redux, Qlik, and CSS</small>
                     <ul className="p-0 m-0 text-lg list-none sm:list-disc sm:pl-8">
                         <li className="mb-4">
                             Worked on cutting edge technology within the Qlik ecosystem, think tableu, and innovated ontop of the existing system. The Qlik default only support an early adoption of angular 1.X except I figured out a react and redux implementation.
@@ -80,7 +85,8 @@ const WorkSection = () => {
                     <p className="italic">This technology was very new at the time of implementation. It had a big impact on making data more accessible on the web.</p>
                 </li>
                 <li className="m-0 ml-auto mt-8 min-w-[250px] max-w-[600px]">
-                    <h4 className="mt-0 text-2xl">🚕 A to B Uber/Lyft like Application</h4>
+                    <h4 className="mt-0 mb-0 mb-2 text-2xl">🚕 A to B Uber/Lyft like Application</h4>
+                    <small className="block mb-6 font-bold">Angular 1.X, Javascript, PSQL, CSS/SCSS, Python, Flask, CakePHP, PHP, PhoneGap, Cordova, JQuery, and Heroku</small>
                     <ul className="p-0 m-0 text-lg list-none sm:list-disc sm:pl-8">
                         <li className="mb-4">
                             Contributed to an Uber like application that serviced multitudes of clients, such as Facebook, Apple, Nike, and other major transit micro/macro operators.
