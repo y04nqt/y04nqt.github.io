e6c68a5ee628c50d40ab4e9570b45eb2acf02a2c
nnnnQT <cortana@s007xcpu2552.lan>
2023-04-21 22:14:10 -0500

    more styles and componentization

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -2,6 +2,7 @@ import HowThisWasMade from "../components/HowThisWasMade";
 import LilFooter from "../components/LilFooter";
 import MainSection from "../components/MainSection";
 import QuotesSection from "../components/QuotesSection";
+import WakatimeChart from "../components/WakatimeChart";
 import WorkSection from "../components/WorkSection";
 
 export default function MainPage() {
@@ -11,9 +12,7 @@ export default function MainPage() {
       <div className="w-full h-full bg-blue" />
       <QuotesSection />
       <WorkSection />
-      <figure className="w-1/2 px-8 mx-auto mt-10 bg-white bg-opacity-50 rounded-xl">
-        <embed src="https://wakatime.com/share/@y04nqt/325a5df3-3e3f-449f-bfd2-8e9a7de920a2.svg"></embed>
-      </figure>
+      <WakatimeChart />
       <HowThisWasMade />
       <div className="h-[64px]" />
       <LilFooter />
