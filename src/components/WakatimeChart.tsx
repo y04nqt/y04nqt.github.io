ad1e84f10642d0790e713b4ef6cde0f9008258e5
nnnnQT <cortana@s007xcpu2552.lan>
2023-04-21 22:19:21 -0500

    mobile optimized chart

--- src/components/WakatimeChart.tsx
+++ src/components/WakatimeChart.tsx
@@ -1,6 +1,6 @@
 const WakatimeChart = () => {
   return (
-    <figure className="w-1/2 px-8 mx-auto mt-10 bg-white bg-opacity-50 shadow-xl rounded-xl">
+    <figure className="w-[calc(100%-144px)] sm:w-1/2 px-8 mx-auto mt-10 bg-white bg-opacity-50 shadow-xl rounded-xl">
       <embed src="https://wakatime.com/share/@y04nqt/325a5df3-3e3f-449f-bfd2-8e9a7de920a2.svg"></embed>
     </figure>
   );
