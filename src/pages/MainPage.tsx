d3219c089c4410df8fbf75ea8cf4d5d185bf9cfb
nnnnQT <cortana@s007xcpu2552.lan>
2023-04-21 22:10:24 -0500

    added my wakatime chart

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -11,6 +11,9 @@ export default function MainPage() {
       <div className="w-full h-full bg-blue" />
       <QuotesSection />
       <WorkSection />
+      <figure className="w-1/2 px-8 mx-auto mt-10 bg-white bg-opacity-50 rounded-xl">
+        <embed src="https://wakatime.com/share/@y04nqt/325a5df3-3e3f-449f-bfd2-8e9a7de920a2.svg"></embed>
+      </figure>
       <HowThisWasMade />
       <div className="h-[64px]" />
       <LilFooter />
