fdfa886fea7d2d3a930ffcfe4b5f806a418b6ebd
nnnnQT <cortana@s007xcpu2552.lan>
2023-04-24 18:27:15 -0500

    mobilized slideshow

--- src/components/SlideShow.tsx
+++ src/components/SlideShow.tsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
 
-type TData = { author: string; quote: string, link: string };
+type TData = { author: string; quote: string; link: string };
 interface IProps {
   data: TData[];
 }
@@ -30,7 +30,7 @@ const SlideShow = ({ data }: IProps) => {
 
   return (
     <>
-      <ul className="hidden sm:block relative list-none min-h-[720px] p-0 mt-0 mb-10">
+      <ul className="block relative list-none w-full min-h-[720px] p-0 mt-0 mb-10">
         {data.map((item: TData, idx1) => (
           <li
             key={item.author}
@@ -38,14 +38,14 @@ const SlideShow = ({ data }: IProps) => {
               idx1 === currentSlide ? "" : "right-[1000%]"
             }`}
           >
-            <div className="flex justify-center gap-20 mb-5">
+            <div className="flex justify-center gap-5 mb-5 sm:gap-20">
               <button
                 className="p-2 bg-transparent border-none rounded-xl"
                 onClick={decrementSlide}
                 tabIndex={idx1 !== currentSlide ? -1 : 0}
                 aria-label="slide left"
               >
-                <ChevronLeftIcon className="transition-shadow duration-300 hover:transition-shadow hover:duration-300 shadow hover:shadow-xl !text-6xl rounded-full border-solid border-1 border-[#00000055]" />
+                <ChevronLeftIcon className="transition-shadow duration-300 hover:transition-shadow hover:duration-300 shadow hover:shadow-xl !text-4xl sm:!text-6xl rounded-full border-solid border-1 border-[#00000055]" />
               </button>
               <ol className="flex items-center gap-2 p-0 list-none">
                 {data.map((item, idx) => (
@@ -67,25 +67,28 @@ const SlideShow = ({ data }: IProps) => {
                 tabIndex={idx1 !== currentSlide ? -1 : 0}
                 aria-label="slide right"
               >
-                <ChevronRightIcon className="transition-shadow duration-300 hover:transition-shadow hover:duration-300 shadow hover:shadow-xl !text-6xl rounded-full border-solid border-1 border-[#00000055]" />
+                <ChevronRightIcon className="transition-shadow duration-300 hover:transition-shadow hover:duration-300 shadow hover:shadow-xl !text-4xl sm:!text-6xl rounded-full border-solid border-1 border-[#00000055]" />
               </button>
             </div>
             <div
-              className={`bg-white bg-opacity-70 py-5 min-h-[460px] flex flex-col justify-center items-center`}
+              className={`bg-white bg-opacity-70 py-5 min-h-[360px] relative max-h-[360px] overflow-y-auto flex flex-col justify-center items-center`}
             >
               <p
-                className={`transition duration-300 ${
+                className={`transition absolute lg:relative top-0 duration-300 ${
                   idx1 === currentSlide ? "opacity-100" : "opacity-0"
                 } px-10 mx-auto text-xl md:w-3/4 italic`}
               >
                 "{item.quote}"
-              </p>
               <p
                 className={`transition duration-300 ${
                   idx1 === currentSlide ? "opacity-100" : "opacity-0"
                 } text-lg font-bold`}
               >
-                - <a className="text-black no-underline" href={item.link}>{item.author}</a>
+                -{" "}
+                <a className="text-black no-underline" href={item.link}>
+                  {item.author}
+                </a>
+              </p>
               </p>
             </div>
           </li>
